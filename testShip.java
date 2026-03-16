6764c58bf07d5828556d1160c74d00004586264a
Christopher Muller <112350489+ChristopherMuller@users.noreply.github.com>
2026-03-16 23:21:25 +0200

    Update testShip.java
    Created fillArray() method
    
    - Asked user for number of ships
    
    - Allowed user to choose ship type
    
    - Created objects based on input
    
    - Stored objects in Ship array
    
    Created displayArray() method
    
    - Looped through array
    
    - Printed each object using toString()
    
    Used polymorphism:
    
    - Stored CruiseShip and CargoShip in Ship[]

--- testShip.java
+++ testShip.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class testShip {
+
+    public static Ship[] fillArray(Ship[] arrShips) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many ships? ");
+        int n = input.nextInt();
+        input.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Type (1 Cruise, 2 Cargo): ");
+            int type = input.nextInt();
+            input.nextLine();
+
+            System.out.print("Name: ");
+            String name = input.nextLine();
+
+            System.out.print("Year: ");
+            int year = input.nextInt();
+
+            if (type == 1) {
+                System.out.print("Passengers: ");
+                int p = input.nextInt();
+                arrShips[i] = new CruiseShip(name, year, p);
+            } else {
+                System.out.print("Capacity: ");
+                int c = input.nextInt();
+                arrShips[i] = new CargoShip(name, year, c);
+            }
+        }
+
+        return arrShips;
+    }
+
+    public static void displayArray(Ship[] arrShips) {
+        for (int i = 0; i < arrShips.length; i++) {
+            if (arrShips[i] != null) {
+                System.out.println(arrShips[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Ship[] arrShips = new Ship[10];
+        arrShips = fillArray(arrShips);
+        displayArray(arrShips);
     }
 }
